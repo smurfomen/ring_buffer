410c273e3edc026f1506c7e91b51c23b249809fe
pcuser <vova.skiller@gmail.com>
2023-05-22 16:06:14 +0300

    added support optional read and QByteArray for write if QT_CORE_LIB used. Added ref annotations and global refactor. Added noexcept and const annotations.

--- ringbuffer.h
+++ ringbuffer.h
@@ -3,7 +3,13 @@
 
 #include <array>
 #include <stdexcept>
-#include <limits>
+#ifdef OPTIONAL_INCLUDED
+#include <optional>
+#endif
+
+#ifdef QT_CORE_LIB
+#include <QByteArray>
+#endif
 
 namespace  {
 	/* S must be one of the number series of powers of 2 */
@@ -18,84 +24,144 @@ class ring_buffer
 {
 	static_assert(ring_buffer_size_validator__<S>::value, "S must be one of the number series of powers of 2");
 
-    using index_t = unsigned long long;
-    std::array<T, S> m_storage;
-    size_t m_cwrite {0};
-    size_t m_cread {0};
-    const index_t m_mask = S - 1;
+	using index_type = std::size_t;
+	using value_type = T;
+
+	std::array<T, S> m_storage;
+	index_type m_cwrite {0};
+	index_type m_cread {0};
+	const index_type m_mask = S - 1;
 
 public:
-    ring_buffer()
-        : m_cwrite(0), m_cread(0)
-    { }
+	ring_buffer()
+		: m_cwrite(0), m_cread(0)
+	{ }
 
-    /*! \brief  Read element to\e value from the end of buffer and drop element, returns true if operation was successed. */
-    bool read(T & value)
-    {
-        if(is_empty()) return false;
+	const value_type* raw() const & {
+		return &m_storage[0];
+	}
 
-        value = m_storage[m_cread++ & m_mask];
-        return true;
-    }
+	/*! \brief  Read element to\e value from the end of buffer and drop element, returns true if operation was successed. */
+	bool read(value_type & value) {
+		if(is_empty()) return false;
 
-    /*! \brief  Append \e value to the end of buffer, returns true if operation was successed. */
-    bool write(T value)
-    {
-        if(is_full()) return false;
+		value = m_storage[m_cread++ & m_mask];
+		return true;
+	}
 
-        m_storage[m_cwrite++ & m_mask] = value;
-        return true;
-    }
+#ifdef OPTIONAL_INCLUDED
+	optional<value_type> read() {
+		if(!is_empty())
+			return m_storage[m_cread++ & m_mask];
 
-    /*! \brief  Returns true if buffer is fulled. */
-    bool is_full()
-    { return ((index_t)(m_cwrite - m_cread) & (index_t)~(m_mask)) != 0; }
+		return nullopt;
+	}
+#endif
 
-    /*! \brief  Returns true if buffer is empty. */
-    bool is_empty()
-    { return m_cwrite == m_cread; }
+	/*! \brief  Append \e value to the end of buffer, returns true if operation was successed. */
+	bool write(const value_type & value) {
+		if(is_full()) return false;
 
-    /*! \brief  Returns count of elements into buffer. */
-    index_t count() const
-    { return (m_cwrite - m_cread) & m_mask; }
+		m_storage[m_cwrite++ & m_mask] = value;
+		return true;
+	}
 
-    /*! \brief  Returns size capacity of buffer. */
-    constexpr size_t size() const
-    { return S; }
 
-    /*! \brief  Returns first element from buffer. Don't drop element. May throw OOE exception. */
-    T first() const
-    { return operator[](0); }
+	bool write(value_type && value) {
+		if(is_full()) return false;
 
-    /*! \brief  Returns last element from buffer. May throw OOE exception. */
-    T last() const
-    { return operator[](count() - 1); }
+		m_storage[m_cwrite++ & m_mask] = std::move(value);
+		return true;
+	}
 
-    /*! \brief  Reset count elements. */
-    void clear()
-    { m_cread = 0; m_cwrite = 0; }
+#ifdef QT_CORE_LIB
+	bool write(QByteArray && a) {
+		if(size() - count() < a.size())
+			return false;
 
-    ring_buffer & operator<<(T v)
-    {
-        write(v);
-        return *this;
-    }
+		for(auto && ch : a) {
+			write(ch);
+		}
 
-    T & operator[](index_t i)
-    {
-        if (is_empty() || i > count())
-            throw std::out_of_range("index located in invalid range for access ring buffer");
+		return true;
+	}
+#endif
 
-        return m_storage[(m_cread + i) & m_mask];
-    }
+	bool write(const void * const p, ssize_t len) {
+		if(!len || (size() - count()) < len)
+			return false;
 
-    const T operator[] (index_t i) const
-    {
-        if (is_empty() || i > count())
-            throw std::out_of_range("index located in invalid range for access ring buffer");
+		const value_type * ptr = (const value_type*)p;
 
-        return m_storage[(m_cread + i) & m_mask];
-    }
+		int i = 0;
+		while(i < len) write(ptr[i++]);
+
+		return true;
+	}
+
+	/*! \brief  Returns true if buffer is fulled. */
+	bool is_full() const noexcept
+	{ return ((index_type)(m_cwrite - m_cread) & (index_type)~(m_mask)) != 0; }
+
+	/*! \brief  Returns true if buffer is empty. */
+	bool is_empty() const noexcept
+	{ return m_cwrite == m_cread; }
+
+	/*! \brief  Returns count of elements into buffer. */
+	index_type count() const noexcept
+	{ return (m_cwrite - m_cread) & m_mask; }
+
+	/*! \brief  Returns size capacity of buffer. */
+	constexpr size_t size() const noexcept
+	{ return S; }
+
+	/*! \brief  Returns first element from buffer. Don't drop element. May throw OOE exception. */
+	value_type& first() &
+	{ return operator[](0); }
+
+	/*! \brief  Returns last element from buffer. May throw OOE exception. */
+	value_type& last() &
+	{ return operator[](count() - 1); }
+
+	/*! \brief  Returns first element from buffer. Don't drop element. May throw OOE exception. */
+	const value_type& first() const &
+	{ return operator[](0); }
+
+	/*! \brief  Returns last element from buffer. May throw OOE exception. */
+	const value_type& last() const &
+	{ return operator[](count() - 1); }
+
+	/*! \brief  Reset count elements. */
+	void clear() noexcept
+	{ m_cread = 0; m_cwrite = 0; }
+
+	ring_buffer& operator<<(const value_type & v)
+	{
+		write(v);
+		return *this;
+	}
+
+	ring_buffer& operator<<(value_type && v)
+	{
+		write(std::forward<value_type>(v));
+		return *this;
+	}
+
+	value_type& operator[](index_type i) &
+	{
+		if (is_empty() || i > count())
+			throw std::out_of_range("index located in invalid range for access ring buffer");
+
+		return m_storage[(m_cread + i) & m_mask];
+	}
+
+	const value_type operator[] (index_type i) const &
+	{
+		if (is_empty() || i > count())
+			throw std::out_of_range("index located in invalid range for access ring buffer");
+
+		return m_storage[(m_cread + i) & m_mask];
+	}
 };
 
 #endif // RINGBUFFER_H
